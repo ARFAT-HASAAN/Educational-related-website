3118c259ba76118f7cb0e56eea305d3c4be73009
ARFAT-HASAAN <arfuhtv757@gmail.com>
2021-10-04 23:31:29 +0600

    last commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import logo from './logo.svg';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 import { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,13 +17,39 @@ import Notfound from './Components/notfound/Notfound';
 import Footer from './Components/footer/Footer';
 import Course from './Components/course/Course';
 import Home from './Components/Home/Home';
+import Displyservice from './Displyservice/Displyservice';
+import Banner from './Components/banner/Banner';
 
 
 function App() {
   return (
     <>
-      {/* <Home></Home> */}
-      <Course></Course>
+      <Router>
+        <Home></Home>
+        <Switch>
+          <Route exact path="/">
+            <Banner></Banner>
+            <Displyservice></Displyservice>
+          </Route>
+          <Route path="/home">
+            <Banner></Banner>
+            <Displyservice></Displyservice>
+
+          </Route>
+
+          <Route exact path="/course">
+            <Course></Course>
+          </Route>
+          <Route exact path="/about">
+            <About></About>
+          </Route>
+          <Route path="*">
+            <Notfound></Notfound>
+          </Route>
+        </Switch>
+        <Footer></Footer>
+
+      </Router>
 
 
 
--- src/Components/header/Header.js
+++ src/Components/header/Header.js
@@ -3,45 +3,60 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUserGraduate, } from '@fortawesome/free-solid-svg-icons'
 import Banner from '../banner/Banner';
+import { Link, NavLink } from 'react-router-dom';
+import { Container, Nav, Navbar, Stack } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <div>
-            <nav className="navbar p-3 navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        <FontAwesomeIcon icon={faUserGraduate} />
-                        <b className='text-danger' >D.Learning Institue</b>
-                    </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Features</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link" href="#">Pricing</a>
-                            </li>
-                            <li className="nav-item">
-                                <a className="nav-link disabled">Disabled</a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
 
-            {
-                <Banner></Banner>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home"><FontAwesomeIcon icon={faUserGraduate} />
+                        <b className='text-danger' >D.Learning Institue</b> </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Stack direction="horizontal" gap={5}>
+                                <NavLink
+                                    to="/home"
+                                    activeStyle={{
+                                        fontWeight: "bold",
+                                        color: "blue"
+                                    }}
+                                >
+                                    Home
+                                </NavLink>
+                                <NavLink
+                                    to="/course"
+                                    activeStyle={{
+                                        fontWeight: "bold",
+                                        color: "red"
+                                    }}
+                                >
+                                    Courses
+                                </NavLink>
 
-            }
+                                <NavLink
+                                    to="/about"
+                                    activeStyle={{
+                                        fontWeight: "bold",
+                                        color: "red"
+                                    }}
+                                >
+                                    Aboute
+                                </NavLink>
+
+                            </Stack>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
 
 
-        </div>
+
+
+        </div >
     );
 };
 
